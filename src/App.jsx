3f1dc858eb57c92805ce6294e82a1615c6ed9cb1
Eve Candy Mutunga <100016781+Evecandy@users.noreply.github.com>
2023-05-23 23:54:18 +0300

    Update the API
    Added a working API

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   
   const setter = useCallback(async () => {
     if (country) {
-      const response = await fetch(`http://universities.hipolabs.com/search?country=${country}`)
+      const response = await fetch(`https://universitiesapi.onrender.com/v1/api/universitiessearch?country=${country}`)
       const data = await response.json()
       setUniversities(data)
       
